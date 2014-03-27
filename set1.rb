be9f6f81152840946ba32d02e5039ac1760222c7
Christopher Palmer <snly2386@gmail.com>
2014-03-27 08:58:58 -0500

    Algorithms set 1

--- set1.rb
+++ set1.rb
@@ -1,10 +1,62 @@
 module Set1
   def self.swap_small(array)
+    #This is an example of 0(n)
+    if array.length == 1
+      array
+    elsif array.length == 2
+       array.sort
+    else
+    first = array[0] 
+    min = array.min # --> Minimum Value
+    minindex = array.find_index(array.min) # --> Finds index of the min value
+    array[minindex] = first  #---> Swaps the first element into the min value index
+    array[0] = min # --> Swpas the min value into the first index
+    array
+  end
   end
 
   def self.find_sum_2(array, sum = 0)
+    # This is an example of 0(n^2)
+    if array.empty? 
+      false
+    elsif array.include?(0) 
+      true
+    else
+
+    array.each do |x|
+      if x < 0
+        if array.include?(x.abs)
+          return true
+        end
+      end
+    end
+
+    array.each do |x| 
+    if x + x == 0
+      return true
+    else
+      return false
+    end
+    end
+  end
+end
+
+  def self.find_sum_3(array,sum=0)
+    #This is an example of O(n^3)
+   if array.length == 0
+      return false
+    end
+
+    array.each do |x|
+      array.each do |y|
+        array.each do |z|
+          if (x + y + z == sum)
+            return true
+          end
+        end
+      end
+    end
+      return false
   end
 
-  def self.find_sum_3(array)
-  end
 end
